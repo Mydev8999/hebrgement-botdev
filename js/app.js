dd16c4917a6a5948a89e82245416bf1703b6f4ab
Mydev8999 <124028093+Mydev8999@users.noreply.github.com>
2023-02-26 09:40:31 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,68 @@
+// fonction pour vérifier le contenu un input
+
+function reponse1(){
+    let a = document.getElementById("rep1").value;
+
+    if(a === "html"){
+        alert("super")
+
+    
+    
+    }
+
+    else{
+        alert("raté c'était le html")
+    }
+
+}
+
+function reponse2(){
+    let a = document.getElementById("rep2").value;
+
+    if(a === "<h1>bonjour</h1>"){
+        alert("super")
+
+    
+    
+    }
+
+    else{
+        alert("raté ")
+    }
+
+}
+
+
+function reponse3(){
+    let a = document.getElementById("rep3").value;
+
+    if(a === "<p>lorem40</p>"){
+        alert("super")
+
+    
+    
+    }
+
+    else{
+        alert("raté ")
+    }
+
+}
+
+function reponse4(){
+    let a = document.getElementById("rep4").value;
+
+    if(a === "<h2>sous titre</h2>"){
+        alert("super")
+
+    
+    
+    }
+
+    else{
+        alert("raté ")
+    }
+
+}
+
+
